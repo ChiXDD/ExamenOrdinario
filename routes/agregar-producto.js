be83d0e2bc982daad461fa90666e1a93190c0634
Daniel <dchi.24700@umsa.edu.mx>
2024-06-04 15:47:41 -0600

    Carpeta Routes

--- routes/agregar-producto.js
+++ routes/agregar-producto.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+// Configuración de bodyParser para manejar cargas grandes
+router.use(bodyParser.json({ limit: '50mb' }));
+router.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Ruta para agregar un producto
 router.get('/', async (req, res) => {
     res.render('agregar-producto');
 });
 
-router.post('/', async (req, res) => {
-    const { nombre, cantidad, precio_compra, precio_venta, imagen} = req.body;
-    try {
-        // Agregar producto a la base de datos
-        await productoController.agregarProducto({ nombre, cantidad, precio_compra, precio_venta, imagen}, req.cookies.token);
-        res.redirect('/catalogo');
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Error interno del servidor');
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Asegúrate de que el directorio 'uploads' exista
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 50 * 1024 * 1024 } // 50MB
+});
+
+router.post('/', upload.single('imagen'), async (req, res) => {
+    const { nombre, cantidad, precio_compra, precio_venta } = req.body;
+    const imagenPath = req.file.path; // La ruta del archivo subido
+
+    // Convertir la imagen a base64 antes de enviarla
+    const base64Image = fs.readFileSync(imagenPath, { encoding: 'base64' });
+    const producto = { nombre, cantidad, precio_compra, precio_venta, imagen: base64Image };
+
+    try {
+        await productoController.agregarProducto(producto, req.session.token);
+        res.redirect('/catalogo');
+    } catch (error) {
+        res.status(400).send('Hubo un error al agregar el producto');
+    }
+});
+
 
 module.exports = router;
